13ccb9b511adddc664e3f53357e9b54a6404c8d4
NatanTavares <nattantavares.s15@gmail.com>
2021-06-21 22:48:05 -0300

    feat(component): Create the button component with icon

---
+++ src/components/ButtonIcon/styles.ts
@@ -0,0 +1,31 @@
+import { StyleSheet } from "react-native";
+import { theme } from "../../global/styles/theme";
+
+export const styles = StyleSheet.create({
+  container: {
+    flexDirection: "row",
+    alignItems: "center",
+    width: "100%",
+    height: 56,
+    backgroundColor: theme.colors.primary,
+    borderRadius: 8,
+  },
+  iconWrapper: {
+    justifyContent: "center",
+    alignItems: "center",
+    width: 56,
+    height: 56,
+    borderRightWidth: 1,
+    borderColor: theme.colors.line,
+  },
+  icon: {
+    width: 24,
+    height: 18,
+  },
+  title: {
+    flex: 1,
+    textAlign: "center",
+    fontSize: 15,
+    color: theme.colors.heading,
+  },
+});
