fb861425025288666c605c08ccd6ca9e79226135
NatanTavares <nattantavares.s15@gmail.com>
2021-06-22 23:57:10 -0300

    feat(component): Creates the component responsible for the user's photo

---
+++ src/components/Avatar/styles.ts
@@ -0,0 +1,17 @@
+import { StyleSheet } from "react-native";
+
+export const styles = StyleSheet.create({
+  container: {
+    alignItems: "center",
+    justifyContent: "center",
+    width: 48,
+    height: 48,
+    marginRight: 22,
+    borderRadius: 8,
+  },
+  avatar: {
+    width: 46,
+    height: 46,
+    borderRadius: 8,
+  },
+});
