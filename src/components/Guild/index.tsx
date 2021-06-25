01d5216c8758501f86951634006a880363ab3c9e
NatanTavares <nattantavares.s15@gmail.com>
2021-06-24 23:55:25 -0300

    feat(component): Creates the component

---
+++ src/components/Guild/index.tsx
@@ -0,0 +1,38 @@
+import React from "react";
+import {
+  View,
+  Text,
+  TouchableOpacity,
+  TouchableOpacityProps,
+} from "react-native";
+
+import { GuildType } from "../../@types/global/guild";
+
+import { GuildIcon } from "../GuildIcon";
+
+import { styles } from "./styles";
+import { Feather } from "@expo/vector-icons";
+import { theme } from "../../global/styles/theme";
+
+type GuildProps = TouchableOpacityProps & {
+  guild: GuildType;
+};
+
+export function Guild({ guild, ...rest }: GuildProps) {
+  return (
+    <TouchableOpacity style={styles.container} activeOpacity={0.7} {...rest}>
+      <GuildIcon />
+      <View style={styles.content}>
+        <View>
+          <Text style={styles.title}>{guild.name}</Text>
+
+          <Text style={styles.subtitle}>
+            {guild.owner ? "Administrador" : "Convidado"}
+          </Text>
+        </View>
+      </View>
+
+      <Feather name="chevron-right" size={24} color={theme.colors.heading} />
+    </TouchableOpacity>
+  );
+}
---
+++ src/components/Guild/styles.ts
@@ -0,0 +1,27 @@
+import { StyleSheet } from "react-native";
+import { theme } from "../../global/styles/theme";
+
+export const styles = StyleSheet.create({
+  container: {
+    flexDirection: "row",
+    alignItems: "center",
+    width: "100%",
+    paddingHorizontal: 24,
+  },
+  content: {
+    flex: 1,
+    justifyContent: "center",
+  },
+  title: {
+    marginBottom: 4,
+    fontSize: 18,
+    fontFamily: theme.fonts.title700,
+    color: theme.colors.heading,
+  },
+  subtitle: {
+    marginBottom: 12,
+    fontSize: 13,
+    fontFamily: theme.fonts.text400,
+    color: theme.colors.highlight,
+  },
+});
