733e6a12b183726e226b0dc0974dafd152bb760c
NatanTavares <nattantavares.s15@gmail.com>
2021-06-28 19:28:55 -0300

    feat(home): Implements the methods - handleSelectCategory - handleAppointmentCreate - handleAppointmentDetails - loadAppointmentsStorageDatabase

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -1,74 +1,100 @@
 import React, { useState } from "react";
 import { FlatList, View } from "react-native";
+import { useFocusEffect, useNavigation } from "@react-navigation/native";
+import AsyncStorage from "@react-native-async-storage/async-storage";
 
+import { AppointmentType } from "../../@types/global/appointment";
+import { COLLECTION_APPOINTMENTS } from "../../configs/database";
+
+import { Load } from "../../components/Load";
 import { Profile } from "../../components/Profile";
 import { ButtonAdd } from "../../components/ButtonAdd";
+import { Background } from "../../components/Background";
 import { ListHeader } from "../../components/ListHeader";
 import { Appointment } from "../../components/Appointment";
 import { ListDivider } from "../../components/ListDivider";
 import { CategorySelect } from "../../components/CategorySelect";
 
 import { styles } from "./styles";
+import { useCallback } from "react";
 
 export function Home() {
+  const [loading, setLoading] = useState(true);
   const [category, setCategory] = useState("");
+  const [appointments, setAppointments] = useState<AppointmentType[]>([]);
+  const navigation = useNavigation();
 
-  const appointments = [
-    {
-      id: "1",
-      guild: {
-        id: 1,
-        name: "Lendários",
-        icon: null,
-        owner: true,
-      },
-      category: "1",
-      date: "22/06 às 20:40h",
-      description:
-        "É hoje que vamos chegar ao challenger sem perder uma partida da md10",
-    },
-    {
-      id: "2",
-      guild: {
-        id: 2,
-        name: "Tribo",
-        icon: null,
-        owner: false,
-      },
-      category: "3",
-      date: "22/06 às 20:40h",
-      description:
-        "É hoje que vamos chegar ao challenger sem perder uma partida da md10",
-    },
-  ];
-
-  function handleCategorySelect(categoryId: string) {
+  function handleSelectCategory(categoryId: string) {
     categoryId === category ? setCategory("") : setCategory(categoryId);
   }
 
+  function handleAppointmentCreate() {
+    navigation.navigate("AppointmentCreate");
+  }
+
+  function handleAppointmentDetails(guildSelected: AppointmentType) {
+    navigation.navigate("AppointmentDetails", { guildSelected });
+  }
+
+  async function loadAppointmentsStorageDatabase() {
+    const response = await AsyncStorage.getItem(COLLECTION_APPOINTMENTS);
+    const storedAppointments: AppointmentType[] = response
+      ? JSON.parse(response)
+      : [];
+
+    if (category) {
+      setAppointments(
+        storedAppointments.filter((item) => item.category === category)
+      );
+    } else {
+      setAppointments(storedAppointments);
+    }
+
+    setLoading(false);
+  }
+
+  useFocusEffect(
+    useCallback(() => {
+      setLoading(true);
+      loadAppointmentsStorageDatabase();
+    }, [category])
+  );
+
   return (
-    <View style={styles.container}>
+    <Background>
       <View style={styles.header}>
         <Profile />
-        <ButtonAdd />
+        <ButtonAdd onPress={handleAppointmentCreate} />
       </View>
 
       <CategorySelect
         categorySelected={category}
-        setCategory={handleCategorySelect}
+        onSelectCategory={handleSelectCategory}
       />
 
-      <View style={styles.content}>
-        <ListHeader title="Partidas agendadas" subtitle="Total 6" />
+      <ListHeader
+        title="Partidas agendadas"
+        subtitle={`Total ${appointments.length}`}
+      />
+
+      {loading ? (
+        <Load />
+      ) : (
         <FlatList
           style={styles.matches}
+          contentContainerStyle={{ paddingBottom: 70 }}
           showsHorizontalScrollIndicator={false}
           ItemSeparatorComponent={() => <ListDivider />}
           data={appointments}
-          keyExtractor={(item) => item.id}
-          renderItem={({ item }) => <Appointment data={item} />}
+          keyExtractor={(_, index) => String(index)}
+          renderItem={({ item }) => (
+            <Appointment
+              data={item}
+              onPress={() => handleAppointmentDetails(item)}
+            />
+          )}
         />
-      </View>
-    </View>
+      )}
+    </Background>
   );
 }
--- src/screens/Home/styles.ts
+++ src/screens/Home/styles.ts
@@ -2,9 +2,6 @@ import { StyleSheet } from "react-native";
 import { getStatusBarHeight } from "react-native-iphone-x-helper";
 
 export const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-  },
   header: {
     flexDirection: "row",
     justifyContent: "space-between",
@@ -12,9 +9,6 @@ export const styles = StyleSheet.create({
     paddingHorizontal: 24,
     marginTop: getStatusBarHeight() + 26,
     marginBottom: 42,
-  },
-  content: {
-    marginTop: 42,
   },
   matches: {
     marginTop: 24,
