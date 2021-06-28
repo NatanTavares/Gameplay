c9b7c18d585acc9fe587bf9ed669dda56cf7ed41
NatanTavares <nattantavares.s15@gmail.com>
2021-06-28 19:41:09 -0300

    refactor(component): The component starts receiving the guild image dynamically

--- src/components/GuildIcon/index.tsx
+++ src/components/GuildIcon/index.tsx
@@ -1,11 +1,26 @@
 import React from "react";
-import { Image } from "react-native";
+import { Image, View } from "react-native";
 
+const { CDN_IMAGE } = process.env;
+
+import DiscordSvg from "../../assets/discord.svg";
 import { styles } from "./styles";
 
-type GuildIconProps = {};
+type GuildIconProps = {
+  guildId: string;
+  iconId: string | null;
+};
 
-export function GuildIcon({}: GuildIconProps) {
-  const uri = "https://github.com/NatanTavares.png";
-  return <Image style={styles.image} source={{ uri }} resizeMode="cover" />;
+export function GuildIcon({ guildId, iconId }: GuildIconProps) {
+  const uri = `${CDN_IMAGE}/icons/${guildId}/${iconId}.png`;
+
+  return (
+    <View style={styles.container}>
+      {!!iconId ? (
+        <Image style={styles.image} source={{ uri }} resizeMode="cover" />
+      ) : (
+        <DiscordSvg width={40} height={40} />
+      )}
+    </View>
+  );
 }
--- src/components/GuildIcon/styles.ts
+++ src/components/GuildIcon/styles.ts
@@ -1,9 +1,18 @@
 import { StyleSheet } from "react-native";
+import { theme } from "../../global/styles/theme";
 
 export const styles = StyleSheet.create({
-  image: {
+  container: {
+    alignItems: "center",
+    justifyContent: "center",
     width: 66,
     height: 66,
     borderRadius: 8,
+    overflow: "hidden",
+    backgroundColor: theme.colors.discord,
+  },
+  image: {
+    width: 66,
+    height: 66,
   },
 });
